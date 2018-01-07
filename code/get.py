de829e02bd04d3b61acb409a42ff5ee3f1529df8
Hugo Hörnquist <hugo@hornquist.se>
2018-01-07 12:36:24 +0100

    put account loading into function

--- code/get.py
+++ code/get.py
@@ -11,33 +11,49 @@ import os.path
 
 lat, lng, city = 58.410807, 15.621373, "Linköping"
 
-account_filename = os.path.join (os.environ.get("XDG_DATA_HOME") or os.path.join(os.environ.get("HOME"), ".local/share/"), "jodel/account_data")
+path_prefix = os.environ.get("XDG_DATA_HOME") or \
+        os.path.join(os.environ.get("HOME"), ".local/share/")
 
-try:
-    if os.path.isfile (account_filename):
-        with open (account_filename, "r") as account_file:
-            account_data = eval(account_file.read())
-            account = jodel_api.JodelAccount(lat=lat, lng=lng, city=city, **account_data)
-    else:
-        path = os.path.dirname(account_filename)
-        if not os.path.exists (path):
-            os.makedirs (path)
+def setup_account (lat, lng, city) -> jodel_api.JodelAccount:
 
-        with open (account_filename, "w") as account_file:
-            account = jodel_api.JodelAccount(lat=lat, lng=lng, city=city);
-            account_data = account.get_account_data()
-            account_file.write (str(account_data))
-except Exception as e:
-    print ("Something went wrong with loading or creating the account")
-    print ("Try removing " + account_filename)
-    print ("and run the program again")
-    print ("-" * 50)
-    print (e)
-    exit (1)
+    account_filename = os.path.join (path_prefix, "jodel/account_data")
 
-os.chmod (account_filename, 0o600)
+    try:
+        if os.path.isfile (account_filename):
+            with open (account_filename, "r") as account_file:
+                account_data = eval(account_file.read())
+                account = jodel_api.JodelAccount(
+                        lat=lat,
+                        lng=lng,
+                        city=city,
+                        **account_data)
+        else:
+
+            path = os.path.dirname(account_filename)
+            if not os.path.exists (path):
+                os.makedirs (path)
+
+            with open (account_filename, "w") as account_file:
+                account = jodel_api.JodelAccount(
+                        lat=lat,
+                        lng=lng,
+                        city=city);
+                account_data = account.get_account_data()
+                account_file.write (str(account_data))
+
+    except Exception as e:
+        print ("Something went wrong with loading or creating the account")
+        print ("Try removing " + account_filename)
+        print ("and run the program again")
+        print ("-" * 50)
+        print (e)
+        exit (1)
+
+    os.chmod (account_filename, 0o600)
+
+    return account
 
 #def do(account):
 #main():
-    #status, data = account.get_posts_recent()
-    #return status, data
+        #status, data = account.get_posts_recent()
+        #return status, data
