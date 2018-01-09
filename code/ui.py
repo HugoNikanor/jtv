a00629408dacda72aa4d81fec79745e1550e7268
Hugo Hörnquist <hugo@hornquist.se>
2018-01-09 02:59:22 +0100

    Images posts now marked

--- code/ui.py
+++ code/ui.py
@@ -11,14 +11,29 @@ def count_char (str, chr):
     return count
 
 class JodelWindow:
-    def __init__ (s, stdscr, jodel):
-        my, mx = stdscr.getmaxyx ()
+    def __init__ (s, scr, jodel, x=0, y=0):
+
+        # Mostly for debugging
+        s.jodel = jodel
+
+        # I want types...
+        if jodel.get("image_url"):
+            type = "IMAGE"
+            s.msg = "[IMAGE POST]\n" + jodel["message"]
+        else:
+            type = "TEXT"
+            s.msg = jodel["message"]
+
+        # ---------------------------------
+
+        s.y, s.x = x, y
+
+        my, mx = scr.getmaxyx ()
         
-        s.msg = jodel["message"]
         s.height = max(3, count_char (s.msg, "\n")) + 2
-        s.width = int(mx / 2)
-        s.y, s.x = 4, 13
-        s.win = curses.newwin (s.height, s.width, s.y, s.x)
+        s.width = mx
+        # [0, inf)
+        s.win = scr.derwin (s.height, s.width, y, x)
 
         s.win.border ()
         s.win.vline (1, 6, curses.ACS_SBSB, s.height - 2)
@@ -30,24 +45,60 @@ class JodelWindow:
         s.content_win = s.win.derwin (s.height - 2, s.width - 8, 1, 7)
         cw_h, cw_w = s.content_win.getmaxyx ()
         str_len = cw_h * cw_w
+
+
         s.content_win.addstr (s.msg [:str_len - 2])
 
         s.win.refresh ()
 
 def main (stdscr):
-    curses.noecho ()
-    curses.cbreak ()
-    stdscr.keypad (True)
+    # wrapper does this
+    #curses.noecho ()
+    #curses.cbreak ()
+    #stdscr.keypad (True)
     stdscr.refresh ()
+
+    my, mx = stdscr.getmaxyx ()
+    post_win = curses.newwin(my - 1, int (mx * 3/4), 0, 0)
 
     a = get.account
     status, posts = a.get_posts_recent ()
-    jwin = JodelWindow(stdscr, posts["posts"][0])
 
+    total_height = 1
+    post_windows = []
+    for post in posts["posts"][:6]:
+        jwin = JodelWindow (post_win, post, y=total_height)
+        post_windows += [jwin]
+        total_height += jwin.height
+
+    current_post = 0
+    current_jodel = 0
+
+    #jwin = JodelWindow (post_win, (posts["posts"][current_jodel]))
+    post_win.refresh ()
     stdscr.refresh ()
-    stdscr.getkey ()
 
-    curses.endwin ()
+    while True:
+
+        post_windows [current_jodel].win.standend ()
+
+        c = stdscr.getkey ()
+        if c == 'q':
+            return
+        if c == 'j':
+            current_jodel += 1
+        if c == 'k':
+            current_jodel -= 1
+
+        # Does this even do anything?
+        post_windows [current_jodel].win.standout ()
+        post_windows [current_jodel].win.refresh ()
+
+        #post_win.mvwin (current_line, 0)
+        #jwin.win.erase ()
+        #jwin = JodelWindow (post_win, (posts["posts"][current_jodel]))
+        post_win.refresh ()
+    #curses.endwin ()
 
 if __name__ == "__main__":
     curses.wrapper (main)
